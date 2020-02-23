2b03bdd3b2d5614130af5133f3b1090f498582d9
Tobi-De <tobidegnon@protonmail.com>
2020-02-23 21:59:03 +0100

    comment the unique constraint on victim

--- cjvr/forms.py
+++ cjvr/forms.py
@@ -1,21 +1,34 @@
 from django import forms
 
+from .models import Victim, Plaintiff, Testimony, Task, Report
+
 
 class VictimCreationForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Victim
+        fields = ['first_name', 'last_name', 'age', 'sexe', 'religion', 'address', 'aggression_place', 'status',
+                  'aggressions']
 
 
 class PlaintiffCreationForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Plaintiff
+        fields = ['first_name', 'last_name', 'age', 'sexe', 'religion', 'address', 'contact']
 
 
 class TestimonyCreationForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Testimony
+        fields = ['description']
 
 
 class ReportCreationForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Report
+        fields = ['content']
 
 
 class TaskCreationForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Task
+        fields = ['name', 'description']
--- cjvr/views.py
+++ cjvr/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
-from .models import Plaintiff
-from .selectors import depositions_get_by_plaintiff
+from .forms import (TestimonyCreationForm, VictimCreationForm, PlaintiffCreationForm)
 
 
 def home(request):
-    plaintiffs = depositions_get_by_plaintiff(fetched_by=Plaintiff.objects.first())
-    return render(request, 'cjvr/index.html', {'plaintiffs': plaintiffs})
+    if request.method == "POST":
+        t_form = TestimonyCreationForm(request.POST)
+        v_form = VictimCreationForm(request.POST)
+        p_form = PlaintiffCreationForm(request.POST)
+        if t_form.is_valid() and v_form.is_valid() and p_form.is_valid():
+            pass
+    else:
+        t_form = TestimonyCreationForm()
+        v_form = VictimCreationForm()
+        p_form = PlaintiffCreationForm()
+    context = {
+        "p_form": p_form,
+        "t_form": t_form,
+        "v_form": v_form
+    }
+    return render(request, 'cjvr/index.html', context)
