7aff7f912db169c1d1e829b8028b01262c1cf33a
Tobi-De <tobidegnon@protonmail.com>
2020-02-24 20:25:49 +0100

    remove trailing whitespace

--- cjvr/urls.py
+++ cjvr/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('plaintiffs/', PlaintiffsList.as_view(), name="plaintiffs"),
     path('plaintiffs/<int:pk>/', PlaintiffDetail.as_view(), name="plaintiff-detail"),
     path('register/testimony/', register_testimony, name="register-testimony"),
-    path('statistics /', statistics_graph, name="statistics-graph"),
+    path('statistics/', statistics_graph, name="statistics-graph"),
 ]
