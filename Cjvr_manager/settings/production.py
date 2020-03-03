567180205bd584bf78f6099c3100b9dc5f1efbf1
Tobi-De <tobidegnon@protonmail.com>
2020-03-03 02:40:45 +0100

    change allowed host in production file

--- Cjvr_manager/settings/production.py
+++ Cjvr_manager/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS.append(".herokuapp.com")
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
