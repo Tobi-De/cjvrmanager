37bf1d0453cd5293d4b2fff9297c727cdc420a66
Tobi-De <tobidegnon@protonmail.com>
2020-03-03 02:30:21 +0100

    setup multiple settings file

--- Cjvr_manager/settings/base.py
+++ Cjvr_manager/settings/base.py
@@ -12,27 +12,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import dj_database_url
-import django_heroku
-import dotenv
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'g+6qh_$($%1!+@)ffquby$k6!g^+2#wp8nwv9tdrh2+&ip^!t)'
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get("DEBUG") == "True")
-
-ALLOWED_HOSTS = [".herokuapp.com", "localhost"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -46,14 +32,12 @@ INSTALLED_APPS = [
     'crispy_forms',
     'cjvr.apps.CjvrConfig',
     'users.apps.UsersConfig',
-    'debug_toolbar',
     'chartjs',
     'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,29 +70,6 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -136,10 +97,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'testimonies'
 
 LOGIN_URL = 'login'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
-
-if DEBUG:
-    del DATABASES['default']['OPTIONS']['sslmode']
---
+++ Cjvr_manager/settings/production.py
@@ -0,0 +1,31 @@
+import dj_database_url
+import django_heroku
+
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS.append(".herokuapp.com")
+
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
